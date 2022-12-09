ddb241b24e2b73d754dd90e81d68b12fcede218f
CoraNake <118303388+CoraNake@users.noreply.github.com>
2022-12-08 14:34:41 -1000

    1 file modified, 1 added

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,50 +1,37 @@
 import * as React from 'react';
+import React, { useRef, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/effect-fade';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/pagination";
 
-import '../src/styles.css';
+import "./styles.css";
 
 // import required modules
-import { EffectFade, Navigation, Pagination } from 'swiper';
-import { getDataFromFirebase } from '../database/getDataFromFirebase';
+import { Pagination } from "swiper";
 
 export default function App() {
-  const [data, setData] = React.useState([]);
-
-  React.useEffect(() => {
-    (async () => {
-      const response = await getDataFromFirebase('header-carrousel');
-
-      console.log(response);
-      setData(response);
-    })();
-  }, []);
-
   return (
-    <Swiper
-      spaceBetween={30}
-      effect={'fade'}
-      navigation={true}
-      pagination={{
-        clickable: true,
-      }}
-      modules={[EffectFade, Navigation, Pagination]}
-      className="mySwiper"
-    >
-      {data.map((slide) => (
-        <SwiperSlide>
-          <img
-            src={slide.url}
-            style={{ height: '30rem', minWidth: '100%', objectFit: 'cover' }}
-          />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <>
+      <Swiper
+        pagination={{
+          dynamicBullets: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
   );
 }
